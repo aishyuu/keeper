4cd44ce3ca1bd7bf194a273af8daba61130e5a4a
Ivan Matias <ivanmatias1331@gmail.com>
2022-07-26 04:21:12 -0700

    Final commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,44 @@
 import React from 'react';
+import Header from './Header'
+import Footer from './Footer'
+import Note from './Note'
+import CreateArea from './CreateArea'
 
 function App() {
+    const [notes, setNotes] = React.useState([]);
+
+    function addNote(newNote) {
+        setNotes(prevNotes => {
+            return[...prevNotes, newNote];
+        });
+    }
+
+    function deleteNote(id) {
+        setNotes(prevNotes => {
+            return prevNotes.filter((noteItem,index) => {
+                return index !== id
+            })
+        })
+    }
+
     return(
-        <h1>Test</h1>
+        <div>
+            <Header />
+            <CreateArea onAdd={addNote} />
+            {notes.map((noteItem, index) => {
+                return(
+                    <Note 
+                        key={index}
+                        id={index}
+                        title={noteItem.title}
+                        content={noteItem.content}
+                        onDelete={deleteNote}
+                    />
+                )
+            })}
+
+            <Footer />
+        </div>
     )
 }
 

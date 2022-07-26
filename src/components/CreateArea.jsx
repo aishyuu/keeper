839a9a4f18ca31a3891238c50a25c6138e9c7875
Ivan Matias <ivanmatias1331@gmail.com>
2022-07-26 04:49:42 -0700

    Styling bonus update using Material UI

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import AddIcon from '@mui/icons-material/Add';
+import { Fab } from '@mui/material';
+import Zoom from '@mui/material/Zoom';
+
 
 function CreateArea(props) {
     const [note, setNote] = React.useState({
         title: "",
         content: ""
     });
+
+    const [isExpanded, setExpanded] = React.useState(false)
 
     function handleChange(event) {
         const {name, value} = event.target;
@@ -26,23 +32,34 @@ function CreateArea(props) {
         event.preventDefault()
     }
 
+    function expand(){
+        setExpanded(true)
+    }
+
     return(
         <div>
             <form>
-                <input 
+                {isExpanded ? <input 
                     name="title"
                     onChange={handleChange}
                     value={note.title}
                     placeholder="Title"
-                />
+                /> : null}
+                
                 <textarea
                     name="content"
+                    onClick={expand}
                     onChange={handleChange}
                     value={note.content}
                     placeholder="Take a note..."
-                    rows="3"
+                    rows={isExpanded ? 3 : 1}
                 />
-                <button onClick={submitNote}>Add</button>
+                <Zoom in={isExpanded}>
+                    <Fab onClick={submitNote}>
+                        <AddIcon />
+                    </Fab>
+                </Zoom>
+                
             </form>
         </div>
     )
